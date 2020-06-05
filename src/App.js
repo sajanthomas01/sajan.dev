7a5df746c305cbc9bd6cc102d00f69f3dd0a16bd
sajanthomas01 <sajanthomas93@gmail.com>
2020-06-05 17:00:19 +0530

    initial push

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+function App() {
+  return (
+    <section>
+      <div className="row sam-center">
+        <h1>Sajan Thomas</h1>
+
+        <div className="sam" >
+          <a style={{ color: '#fff', textDecoration: 'none' }} href="https://www.linkedin.com/in/sajanthomas01/">
+            <i className="fa fa-linkedin-square" style={{ fontSize: 25, }}></i>
+          </a>
+
+          <a style={{ color: '#fff', textDecoration: 'none' }} href="https://stackoverflow.com/users/8515675/sajanthomas01">
+            <i className="fa fa-stack-overflow" style={{ fontSize: 25, }}></i>
+          </a>
+
+          <a style={{ color: '#fff', textDecoration: 'none' }} href="https://github.com/sajanthomas01">
+            <i className="fa fa-github-square" style={{ fontSize: 25, }}></i>
+          </a>
+
+          <a style={{ color: '#fff', textDecoration: 'none' }} href="https://www.youtube.com/c/sajanthomas01">
+            <i className="fa fa-youtube-square" style={{ fontSize: 25, }}></i>
+          </a>
+
+          <a style={{ color: '#fff', textDecoration: 'none' }} href="https://medium.com/@sajanthomas01">
+            <i className="fa fa-medium" style={{ fontSize: 25, }}></i>
+          </a>
+
+          <a style={{ color: '#fff', textDecoration: 'none' }} href="https://twitter.com/@sajanthomas01">
+            <i className="fa fa-twitter" style={{ fontSize: 25, }}></i>
+          </a>
+          
+          <i className="fa fa-skype" style={{ fontSize: 25, }} title="live:sajan.t"></i>
+        </div>
+
+        <div><br />
+          <a href="mailto:info@sajan.dev?Subject=Hello%20Sajan" target="_top" style={{ color: '#fff', fontSize: 15, textDecoration: 'none' }}>
+            <i className="fa fa-envelope" style={{ fontSize: 20, }}></i> Say Hello (info@sajan.dev)</a><br /><br />
+        </div>
+
+        <div>
+          <p>[ Node JS, React Native, React, ASK(Alexa), Python ]</p>
+        </div>
+
+        <div className="absolute">
+          <p>sajanthomas01</p>
+        </div>
+
+      </div>
+    </section>
+  );
+}
+
+export default App;
